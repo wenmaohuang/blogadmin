4c1363159f42652c322ab54f7e9f96c909694666
wen <wenmaohuang@hotmail.com>
2020-06-10 00:50:17 +0800

    1

--- client/api/fyyd.js
+++ client/api/fyyd.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// axios.defaults.baseURL = 'http://localhost'
-axios.defaults.baseURL = 'http://fyyd.vip'
+axios.defaults.baseURL = 'http://localhost'
+// axios.defaults.baseURL = 'http://fyyd.vip'
 axios.defaults.withCredentials = true
 axios.defaults.headers.post["Content-Type"]="application/x-www-from-urlencoded"
 
