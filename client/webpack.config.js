b4f8aa29d9dca71bbec41210d3c09d573368f8c0
wen <wenmaohuang@hotmail.com>
2020-06-10 19:54:46 +0800

    2

---
+++ client/webpack.config.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//     // webpack4需要添加这个配置，development为开发环境，production为生产环境
+//     // mode: "development",
+//     // entry:  __dirname + "/app/main.js", // 之前提到的唯一入口文件
+//     output: {
+//         path: __dirname + "/common", // 打包后的文件存放的地方
+//         // filename: "" // 打包后输出文件的文件名
+//     }
+// }
